9799937c40aefaaaac8bfbdeb3d8880132045086
Deivis Alvarez <defaaldi@gmail.com>
2024-09-27 17:34:18 -0500

    ApiCoffeeCarruselBasicComponentSession20

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { ApiCoffeeCarruselBasic } from './components/ApiCoffeeCarruselBasic'
+
+
+function App() {
+  return (
+      <div>
+        <  ApiCoffeeCarruselBasic />
+      </div>
+  )
+}
+export default App
